57d9111ffce91648eaff5ded54849dcc27035fb8
hacktune <hacktune@gmail.com>
2012-11-29 19:44:51 +0900

    Update srv.c

--- srv.c
+++ srv.c
@@ -1,46 +1,67 @@
-/*
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/queue.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
 #include <event.h>
 #include <evhttp.h>
-
-void generic_handler(struct evhttp_request *req, void *arg)
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <pthread.h>
+ 
+#define BUFSIZE 4096
+#define NUMCONNS 2000
+#define SERVERADDR "127.0.0.1"
+#define SERVERPORT 80
+#define SLEEP_MS 10
+ 
+char buf[BUFSIZE];
+ 
+int bytes_recvd = 0;
+int chunks_recvd = 0;
+int closed = 0;
+int connected = 0;
+ 
+// called per chunk received
+void chunkcb(struct evhttp_request * req, void * arg)
 {
-	struct evbuffer *buf;
-	struct evkeyvalq *input_headers;
-	struct evkeyvalq *output_headers;
-	struct evkeyval *header;
-	FILE* file=fopen("/new/tmp.txt","ra+");
-	char body;
-	buf = evbuffer_new();
-
-	if (buf == NULL)
-		{err(1, "failed to create response buffer");
-	input_headers = req->input_headers;
-
-	TAILQ_FOREACH(header,input_headers,next) {
-		evbuffer_add_printf(buf,"%s :", header->key);
-		evbuffer_add_printf(buf,"%s \n", header->value);
-	}
-	while((body = fgetc(file)) != EOF)
-	{
-		evbuffer_add_printf(buf,"%c",body);
-	}
-	evbuffer_add_printf(buf, "Requested: %sn", evhttp_request_uri(req));
-	evhttp_add_header(req->output_headers,"Server","BOB");
-	evhttp_send_reply(req, HTTP_OK, "OK", buf);
+    int s = evbuffer_remove( req->input_buffer, &buf, BUFSIZE );
+    //printf("Read %d bytes: %s\n", s, &buf);
+    bytes_recvd += s;
+    chunks_recvd++;
+    if(connected >= NUMCONNS && chunks_recvd%10000==0)
+        printf(">Chunks: %d\tBytes: %d\tClosed: %d\n", chunks_recvd, bytes_recvd, closed);
 }
+ 
+// gets called when request completes
+void reqcb(struct evhttp_request * req, void * arg)
+{
+    closed++;
+}
+ 
 int main(int argc, char **argv)
 {
-	struct evhttp *httpd;
-	event_init();
-	httpd = evhttp_start("0.0.0.0", 8080);
-	evhttp_set_gencb(httpd, generic_handler, NULL);
-	event_dispatch();
-	evhttp_free(httpd); 
-	return 0;
+    event_init();
+    struct evhttp *evhttp_connection;
+    struct evhttp_request *evhttp_request;
+    char path[32]; // eg: "/test/123"
+    int i,octet;
+        for(i=1;i<=NUMCONNS;i++) {
+            evhttp_connection = evhttp_connection_new(SERVERADDR, SERVERPORT);
+            evhttp_request = evhttp_request_new(reqcb, NULL);
+            evhttp_request->chunk_cb = chunkcb;
+            sprintf(&path, "/test/%d", ++connected);
+            if(i%100==0)  printf("Req: %s\t->\t%s\n", "/test", &path);
+            evhttp_make_request( evhttp_connection, evhttp_request, EVHTTP_REQ_GET, path );
+            // evhttp_connection_set_timeout(evhttp_request->evcon, 864000);
+            event_loop( EVLOOP_NONBLOCK );
+            if( connected % 200 == 0 )
+                printf("\nChunks: %d\tBytes: %d\tClosed: %d\n", chunks_recvd, bytes_recvd, closed);
+            usleep(SLEEP_MS*1000);
+        }
+    event_dispatch();
+    return 0;
 }
-*/
